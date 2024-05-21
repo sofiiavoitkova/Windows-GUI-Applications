da8bb1d6a3ea40ed77769175a2fbe5f811fe14eb
Sofiia <sofiiavoitkova@gmail.com>
2024-05-21 22:48:20 +0200

    doc: add StandartCalculator folder

---
+++ StandartCalculator/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+    
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+    
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxNumber.Location = new System.Drawing.Point(60, 55);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(273, 39);
+            this.textBoxNumber.TabIndex = 0;
+            this.textBoxNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 613);
+            this.Controls.Add(this.textBoxNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNumber;
+       
+    }
+}
+
