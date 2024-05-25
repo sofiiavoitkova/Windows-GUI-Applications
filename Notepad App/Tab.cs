b9993feb51167eea1a005cb3ccd1cb57d149cd89
Sofiia <sofiiavoitkova@gmail.com>
2024-05-25 22:57:10 +0200

    doc: add Tab.cs

---
+++ Notepad App/Tab.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class Tab : TabPage /*UserControl*/
+    {
+        public Tab(string name)
+        {
+            InitializeComponent();
+            Text = name;
+        }
+
+        public TextBox TabTextBox
+        {
+            get { return textBoxText; }
+        }
+    }
+}
