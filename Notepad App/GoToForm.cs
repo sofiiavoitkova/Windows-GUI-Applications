b24f081bae5d154f9fd0a3e9ed80f0757b4477c6
Sofiia <sofiiavoitkova@gmail.com>
2024-05-28 08:34:04 +0200

    doc: add GoToForm.cs

---
+++ Notepad App/GoToForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class GoToForm : Form
+    {
+        public int SelectedLineNumber { get; private set; }
+
+        public GoToForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (int.TryParse(textBoxLine.Text, out int lineNumber))
+                {
+                    SelectedLineNumber = lineNumber;
+                    DialogResult = DialogResult.OK;
+                    Close(); 
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a valid number!", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
