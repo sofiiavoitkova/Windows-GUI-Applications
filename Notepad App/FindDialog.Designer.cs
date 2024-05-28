8604bc772f5690755b203830c5c180a7bd804791
Sofiia <sofiiavoitkova@gmail.com>
2024-05-28 08:35:18 +0200

    doc: add FindDialog.Designer.cs

---
+++ Notepad App/FindDialog.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Notepad
+{
+    partial class FindDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.btnFindNext = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(103, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find what:";
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(211, 88);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(292, 26);
+            this.txtFind.TabIndex = 1;
+            // 
+            // btnFindNext
+            // 
+            this.btnFindNext.Location = new System.Drawing.Point(380, 189);
+            this.btnFindNext.Name = "btnFindNext";
+            this.btnFindNext.Size = new System.Drawing.Size(140, 36);
+            this.btnFindNext.TabIndex = 2;
+            this.btnFindNext.Text = "Find Next";
+            this.btnFindNext.UseVisualStyleBackColor = true;
+            this.btnFindNext.Click += new System.EventHandler(this.btnFindNext_Click_1);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(107, 189);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(140, 36);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // FindDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 313);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFindNext);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.label1);
+            this.Name = "FindDialog";
+            this.Text = "FindDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.Button btnFindNext;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
