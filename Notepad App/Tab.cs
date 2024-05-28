842d5de5e6adeae119c7a39a66edd9f972ff1f43
Sofiia <sofiiavoitkova@gmail.com>
2024-05-28 08:30:39 +0200

    feat: implement status bar update with line and column details

--- Notepad App/Tab.cs
+++ Notepad App/Tab.cs
@@ -1,26 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Notepad
 {
-    public partial class Tab : TabPage /*UserControl*/
+    public partial class Tab : TabPage
     {
-        public Tab(string name)
+        private Form1 parentForm;
+
+        public Tab(string name, Form1 parentForm)
         {
             InitializeComponent();
             Text = name;
+            this.parentForm = parentForm;
+            TabTextBox.TextChanged += TabTextBox_TextChanged;
+        }
+
+        private void TabTextBox_TextChanged(object sender, EventArgs e)
+        {
+            UpdateStatusBar();
         }
 
         public TextBox TabTextBox
         {
             get { return textBoxText; }
+        }
+
+        private void UpdateStatusBar()
+        {
+            int currentLine = TabTextBox.GetLineFromCharIndex(TabTextBox.SelectionStart) + 1;
+            int currentColumn = TabTextBox.SelectionStart - TabTextBox.GetFirstCharIndexOfCurrentLine() + 1;
+            parentForm.UpdateStatusBarText($"Ln: {currentLine}, Col: {currentColumn}");
         }
     }
 }
