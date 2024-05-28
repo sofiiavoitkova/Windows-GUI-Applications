7ce1732d74c4dd26d993e28e8ada7d21395b44ef
Sofiia <sofiiavoitkova@gmail.com>
2024-05-28 08:32:59 +0200

    doc: add Tab.Designer.cs

---
+++ Notepad App/Tab.Designer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Notepad
+{
+    partial class Tab
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxText.Location = new System.Drawing.Point(0, 0);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(395, 324);
+            this.textBoxText.TabIndex = 0;
+            // 
+            // Tab
+            // 
+          
+            this.Controls.Add(this.textBoxText);
+            this.Name = "Tab";
+            this.Size = new System.Drawing.Size(395, 324);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxText;
+    }
+}
