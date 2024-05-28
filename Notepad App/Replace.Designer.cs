40f948d38923bcb60caa96ae9bdcdf4188ae5703
Sofiia <sofiiavoitkova@gmail.com>
2024-05-28 08:36:35 +0200

    doc: add Replace.Designer.cs

---
+++ Notepad App/Replace.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Notepad
+{
+    partial class Replace
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBoxFind = new System.Windows.Forms.MaskedTextBox();
+            this.TextBoxReplace = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(36, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find What";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(36, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Replace With";
+            // 
+            // TextBoxFind
+            // 
+            this.TextBoxFind.Location = new System.Drawing.Point(167, 36);
+            this.TextBoxFind.Name = "TextBoxFind";
+            this.TextBoxFind.Size = new System.Drawing.Size(229, 26);
+            this.TextBoxFind.TabIndex = 2;
+            // 
+            // TextBoxReplace
+            // 
+            this.TextBoxReplace.Location = new System.Drawing.Point(167, 90);
+            this.TextBoxReplace.Name = "TextBoxReplace";
+            this.TextBoxReplace.Size = new System.Drawing.Size(229, 26);
+            this.TextBoxReplace.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Find Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(440, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Replace";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(440, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Replace All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(440, 195);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 37);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Replace
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 333);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBoxReplace);
+            this.Controls.Add(this.TextBoxFind);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Replace";
+            this.Text = "Replace";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox TextBoxFind;
+        private System.Windows.Forms.MaskedTextBox TextBoxReplace;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
